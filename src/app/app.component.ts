acad772ae4f5cdca211e614e0324e916db85de1a
fajardoralf <fajardoralf@gmail.com>
2019-03-28 11:59:04 +0100

    my app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Exchange } from "./exchange";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'angularapp';
+export class AppComponent implements OnInit {
+  title = "Exchange Data";
+  results: Exchange[];
+  interval: number;
+  timer: number;
+  progress: number;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.refreshData();
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+
+    this.interval = setInterval(() => {
+      this.refreshData();
+      this.progress = 0;
+    }, 10000);
+
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+    this.timer = setInterval(() => {
+      this.progress += 5;
+    }, 500);
+  }
+
+  refreshData(): void {
+    this.http
+      .get<Exchange[]>(
+        "https://forex.1forge.com/1.0.3/quotes?&api_key=ODvX24ysi1xWofdSljK3bw9ECss7sQqm"
+      )
+      .subscribe(data => {
+        this.results = data;
+      });
+  }
 }
